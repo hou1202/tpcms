4ec1ba9519f281223a90e5f7289930bf63c8ec2a
hou1202 <houss@aliyun.com>
2018-11-29 18:32:25 +0800

    less

--- public/static/index/js/public.js
+++ public/static/index/js/public.js
@@ -2,6 +2,12 @@
  * Created by Administrator on 2018/11/29.
  */
 
+
+/**
+ * @autoAlert   弹窗提示&自动关闭
+ * @param msg   string  提示信息
+ *
+ **/
 function autoAlert(msg) {
     $('#alertInfo p').text(msg);
     $('#alertInfo').show();
