b81cd63be0fa75f404c924d4f3bc2b7e9aa876c5
hou1202 <houss@aliyun.com>
2019-01-10 10:15:39 +0800

    at

--- application/common/controller/Alipay.php
+++ application/common/controller/Alipay.php
@@ -44,7 +44,7 @@ class Alipay
         $aop->charset = $aliConfig['input_charset'];    //编码格式
         $aop->signType = $aliConfig['sign_type'];       //签名方式
         $aop->rsaPrivateKey = $aliConfig['private_key'];    //密钥
-        $aop->alipayrsaPublicKey = $aliConfig['public_key'];    //公钥
+        $aop->alipayrsaPublicKey = $aliConfig['ali_public_key'];    //公钥
 
         //实例化具体API对应的request类,类名称和接口名称对应,当前调用接口名称：alipay.trade.wap.pay  手机网站支付接口
         $request = new \AlipayTradeWapPayRequest();
@@ -63,7 +63,7 @@ class Alipay
         $request->setNotifyUrl($urlConfig['notify_url']);
         $request->setBizContent($request_body);
         //这里和普通的接口调用不同，使用的是sdkExecute
-        $response = $aop->sdkExecute($request);
+        $response = $aop->pageExecute($request);
 
         //htmlspecialchars是为了输出到页面时防止被浏览器将关键参数html转义，实际打印到日志以及http传输不会有这个问题
         return ['resp'=>$response];//就是orderString 可以直接给客户端请求，无需再做处理。
