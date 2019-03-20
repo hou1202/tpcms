e51bbd4ee9d10bae950359d4e5450783a73af4ec
hou1202 <houss@aliyun.com>
2019-03-20 10:48:57 +0800

    api

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -49,7 +49,8 @@ class Index extends BaseController
         $field = ['id','title','info','thumbnail','sell_price','origin_price'];
         $where = [
             ['status','=', 1],
-            ['recom','=', 1]
+            ['recom','=', 1],
+            ['delete_time','=', 0],
         ];
         $goods = Db::table('goods')->field($field)->where($where)->order('id desc')->limit($page*$limit,$limit)->select();
 
@@ -69,14 +70,16 @@ class Index extends BaseController
         return $this->apiJson($data);
     }
 
-    public function goodsDetails($id)
+    public function goodsDetails($id, $user_id='')
     {
         $field = ['id','title','info','thumbnail','banner','origin_price','sell_price','franking','volume','address','content'];
         $where = [
             ['status','=', 1],
-            ['id','=', $id]
+            ['id','=', $id],
+            ['delete_time','=', 0]
         ];
-        /*$where[] = ['id','=', $id];*/
+
+        /*产品详情*/
         $details = Db::table('goods')->field($field)->where($where)->find();
         $details['banner'] = explode('-',$details['banner']);
         $details['content'] = html_entity_decode($details['content']);
@@ -86,13 +89,45 @@ class Index extends BaseController
         foreach($details['banner'] as &$value){
             $value = 'http://www.aoogi.com'.$value;
         }
-
+        /*产品规格参数*/
         $params = Db::table('goods_param')->where('goods_id',$id)->select();
         $spec = Db::table('goods_spec')->where('goods_id',$id)->select();
+
+        /*收藏*/
+        $collect = false;
+        if(!empty($user_id)){
+            $collWhere = [
+                ['user_id','=', $user_id],
+                ['goods_id','=', $id]
+            ];
+            $collRec = Db::table('collect')->where($collWhere)->value('id');
+            if(!empty($collRec)){
+                $collect = true;
+            }
+        }
+
+        /*产品评论*/
+        $comAlias = ['user'=>'u','comments'=>'c'];
+        $comField = ['c.id','c.content','c.img','c.laud','c.star','c.create_time','u.name','u.portrait'];
+        $comJoin = [['user','c.user_id=u.id']];
+        $comOrder = 'c.id desc';
+        $comWhere['c.goods_id'] = ['=',$id];
+        $comWhere['c.delete_time'] = ['=',0];
+
+        $comRec = Db::table('comments')
+            ->alias($comAlias)
+            ->field($comField)
+            ->join($comJoin)
+            ->where($comWhere)
+            ->order($comOrder)
+            ->select();
+
         $resource = [
             'detail' => $details,
             'params' => $params,
-            'spec' => $spec
+            'spec' => $spec,
+            'comment' => $comRec,
+            'isColl' => $collect
         ];
         return $this->apiJson($resource);
     }
--- route/api.php
+++ route/api.php
@@ -11,4 +11,4 @@
 Route::get('api/index/banner','api/index/banner');
 Route::get('api/index/recom','api/index/recom');
 Route::get('api/index/indexGoods/:page/:limit','api/index/indexGoods');
-Route::get('api/index/goods/:id','api/index/goodsDetails');
+Route::get('api/index/goods/:id/[:user_id]','api/index/goodsDetails');
